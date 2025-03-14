3c1509f78d9457372455c2438994cb9da464caef
Code-with-Shravani25 <shravanibudharam25@gmail.com>
2025-03-15 01:34:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"  # Change as needed
+  region = "eu-north-1"  # Change as needed
 }
 
 resource "aws_instance" "my_instances" {
-  ami           = "ami-04b4f1a9cf54c11d0"  # Replace with latest Amazon Linux 2 AMI
-  instance_type = "t2.micro"
+  ami           = "ami-0e0d6e610ffe146fe"  # Replace with latest Amazon Linux 2 AMI
+  instance_type = "t3.micro"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
