d8b38187a51ccc790fa50b2b8dcaa60ea7d39473
Code-with-Shravani25 <shravanibudharam25@gmail.com>
2025-03-15 01:27:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "my_instances" {
   count         = 2  # Launch 2 EC2 instances
   ami           = "ami-04b4f1a9cf54c11d0"  # Replace with latest Amazon Linux 2 AMI
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
