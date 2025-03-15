eb317014987c89a0c8dba43c917060c922d8471e
Code-with-Shravani25 <shravanibudharam25@gmail.com>
2025-03-15 23:32:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,6 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instances" {
-  ami           = "ami-0e0d6e610ffe146fe"  # Replace with latest Amazon Linux 2 AMI
+  ami           = "ami-04b4f1a9cf54c11d0"  # Replace with latest Amazon Linux 2 AMI
   instance_type = "t2.micro"
 }
